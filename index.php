70dce82dc60a2d4aa30cd4a138038c86f6158611
Malikabhc <malika.bouhcine@gmail.com>
2018-07-31 08:52:16 +0200

    exercice3-5

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 3/5</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $months = array ('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre');
+        echo $months[5];
+      ?>
+    </p>
+  </body>
+</html>
